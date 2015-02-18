4d7d924af4cb5cc0830394e414809c8bf83dad08
douglasbolis <douglasbolislima@gmail.com>
2015-02-17 22:00:01 -0200

    Adicionado arquivo principal do leilao

---
+++ tarefa4_leilao.py
@@ -0,0 +1,18 @@
+# Tarefa proposta para desenvolver um leilao de produtos usando dicionario, o programa deve conter tres dicionarios:
+#	- Produtos
+#	- Clientes
+#	- Ofertas
+
+def main():
+	dicProdutos = {}
+	dicClientes = {}
+	dicOfertas = {}
+	
+	
+	
+	return 0
+#fim main
+
+if __name__ == '__main__':
+	main()
+#fim if
