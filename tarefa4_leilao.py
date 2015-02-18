4a344f54a2b06e8aa49e9ffac5028c4481f579c2
douglasbolis <douglasbolislima@gmail.com>
2015-02-17 23:19:43 -0200

    Função de cadastrar lances esta incompleta

--- tarefa4_leilao.py
+++ tarefa4_leilao.py
@@ -12,7 +12,7 @@ def f_cadastraProdutos(dicP):
 		if nome in dicP:
 			print("Produto ja cadastrado!.")
 		else:
-			valor = float(input("Informe o valor do produto %s: ", %(nome)))
+			valor = float(input("Informe o valor do produto %s: " %(nome)))
 			dicP[nome] = valor
 		#fim if
 		print("-----------------------------------------------------")
@@ -30,8 +30,8 @@ def f_cadastraClientes(dicC):
 		if cpf in dicC:
 			print("Cliente ja cadastrado!.")
 		else:
-			nome = input("Informe o nome do cliente %s: ", %(cpf))
-			endereco = input("Informe o endereco do cliente %s: ", %(cpf))
+			nome = input("Informe o nome do cliente %s: " %(cpf))
+			endereco = input("Informe o endereco do cliente %s: " %(cpf))
 			lst = [nome, endereco]
 			dicC[cpf] = lst
 		#fim if
@@ -40,11 +40,37 @@ def f_cadastraClientes(dicC):
 	#fim while	
 #fim funcao
 
+def f_cadastraLances(dicL, dicP, dicC):
+	cpf, nome = "", ""
+	tpl = ()
+	
+	cpf = input("Informe o seu CPF: ")
+	while cpf != "":
+		if cpf not in dicC:
+			print("Cliente nao encontrado!.")
+		else:
+			for i in range(len(dicP)):
+				for j in range(len(dicL)):
+					if dicP[i] in dicL[j][0]:
+						print()
+				#fim for
+			#fim for
+			nome = input("Informe o produto que deseje dar um Lance: ")
+		#fim if
+		cpf = input("Informe o seu CPF: ")
+	#fim while
+	
+#fim funcao
+
 def main():
 	dicProdutos, dicClientes, dicOfertas = {}, {}, {}
 	
 	f_cadastraProdutos(dicProdutos)
 	f_cadastraClientes(dicClientes)
+	f_cadastraLances(dicLances, dicProdutos, dicClientes)
+	
+	print(dicProdutos)
+	print(dicClientes)
 	
 	return 0
 #fim main
